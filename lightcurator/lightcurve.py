9cfa9c59725cd9471a7f748a4f32bcca20bd9ce0
moemyself3 <moemyself3@gmail.com>
2019-04-01 17:58:26 -0500

    Added lacosmic to clean images before alignment

--- lightcurator/lightcurve.py
+++ lightcurator/lightcurve.py
@@ -25,6 +25,7 @@ from astropy.time import Time
 from datetime import datetime
 import matplotlib.pyplot as plt
 import subprocess
+import ccdproc
 
 def plot(object_table):
     """
@@ -81,10 +82,13 @@ def align(object_table):
     date = []
     for item in image_list:
         sci_data = fits.open(item)[0]
-        data.append(np.asarray(sci_data.data))
+        data.append(ccdproc.cosmicray_lacosmic(sci_data.data)[0])
         date.append(sci_data.header['DATE-OBS'])
     object_table['date'] = date
     object_table['data'] = data
+
+    # sort table by date
+    object_table.sort('date')
 
     # pick reference image
     ref_index = len(data)//2
@@ -266,7 +270,7 @@ class mm:
         vmin, vmax = (mean - median, mean + median)
         return vmin, vmax
 
-def makepic(data):
+def makepic(data, filename='frame'):
     """
     Create an image from data frame.
 
@@ -281,7 +285,7 @@ def makepic(data):
     fig = plt.figure()
     plt.imshow(data, cmap='Greys', origin='lower', vmin=vmin, vmax=vmax)
     timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
-    fig.savefig('frame_'+timestamp+'.png')
+    fig.savefig(filename+'_'+timestamp+'.png')
     plt.close(fig)
 
 def astrometry(data):
