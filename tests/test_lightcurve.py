df97182818d849b3196ab98fcdfb633055250a16
moemyself3 <moemyself3@gmail.com>
2019-04-09 13:05:23 -0500

    Added a simple test for matchcat

--- tests/test_lightcurve.py
+++ tests/test_lightcurve.py
@@ -1,20 +1,11 @@
 import unittest
+from lightcurator import lightcurve as lc
 
-class TestStringMethods(unittest.TestCase):
+class TestLightcuratorMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_matchcat(self):
+        cc = lc.matchcat(23)
+        self.assertEqual(cc, 1)
 
 if __name__ == '__main__':
     unittest.main()
